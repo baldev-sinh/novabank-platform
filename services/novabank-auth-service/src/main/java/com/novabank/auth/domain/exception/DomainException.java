f4bdf4c7feb58f0f8c490cfb819cb4910fe75d3c
Baldevsinh Parmar <baldevsinh.parmar@nationalexpress.com>
2026-06-29 01:23:49 +0530

    feat(auth-domain): Implement User Aggregate

---
+++ services/novabank-auth-service/src/main/java/com/novabank/auth/domain/exception/DomainException.java
@@ -0,0 +1,8 @@
+package com.novabank.auth.domain.exception;
+
+public class DomainException extends RuntimeException {
+
+    public DomainException(String message) {
+        super(message);
+    }
+}
