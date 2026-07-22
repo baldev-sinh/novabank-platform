4841a574a061b03c4991550da497e6698614690c
Baldevsinh Parmar <baldevsinh.parmar@nationalexpress.com>
2026-07-22 14:58:32 +0530

    feat(auth): implement logout service

--- frontend/novabank-web/src/app/features/auth/services/authentication.service.ts
+++ frontend/novabank-web/src/app/features/auth/services/authentication.service.ts
@@ -1,14 +1,18 @@
+import { TokenStorageService } from './token-storage.service';
 import { inject, Injectable } from '@angular/core';
 import { LoginRequest, LoginResponse } from '../models';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthenticationService {
   private readonly http = inject(HttpClient);
+  private readonly tokenStorageService = inject(TokenStorageService);
+  private readonly router = inject(Router);
 
   private readonly authUrl = `${environment.api.baseUrl}/auth`;
 
@@ -20,5 +24,16 @@ export class AuthenticationService {
    */
   login(request: LoginRequest): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${this.authUrl}/login`, request);
+  }
+
+  /**
+   * Logs out the current user.
+   *
+   * Clears the stored authentication state and navigates to the login page.
+   */
+  logout(): void {
+    this.tokenStorageService.clear();
+
+    void this.router.navigate(['/login']);
   }
 }
