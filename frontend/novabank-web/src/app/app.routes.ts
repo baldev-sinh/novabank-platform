32d9d28d98d8b9864e86856535e5dba9977d05b0
Baldevsinh Parmar <baldevsinh.parmar@nationalexpress.com>
2026-07-22 17:41:39 +0530

    feat(auth): register guest guard in routes

--- frontend/novabank-web/src/app/app.routes.ts
+++ frontend/novabank-web/src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { MainLayoutComponent } from './layout/main-layout/main-layout.component'
 import { LoginComponent } from './features/auth/pages/login/login.component';
 import { authGuard } from './core/guards/auth.guard';
 import { DashboardComponent } from './features/dashboard/pages/dashboard/dashboard.component';
+import { guestGuard } from './core/guards/guest.guard';
 
 export const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    canActivate: [guestGuard],
   },
   {
     path: '',
