9e9bc33debff012f8d5c8872040847a58a664b6b
Baldevsinh Parmar <baldevsinh.parmar@nationalexpress.com>
2026-08-14 14:17:50 +0530

    fix(auth): handle unauthorized exception

--- services/novabank-auth-service/src/main/java/com/novabank/auth/infrastructure/configuration/SecurityConfiguration.java
+++ services/novabank-auth-service/src/main/java/com/novabank/auth/infrastructure/configuration/SecurityConfiguration.java
@@ -6,12 +6,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -38,6 +40,12 @@ public class SecurityConfiguration {
                 session -> session.sessionCreationPolicy(
                     SessionCreationPolicy.STATELESS
                 )
+            )
+            .exceptionHandling(
+                exceptions -> exceptions
+                    .authenticationEntryPoint(
+                        new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)
+                    )
             )
             .authorizeHttpRequests(
                 auth -> auth
