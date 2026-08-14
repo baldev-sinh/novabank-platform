e77f2af2ee429ec395656366e8f2a8e40cd9cdfd
Baldevsinh Parmar <baldevsinh.parmar@nationalexpress.com>
2026-08-14 15:36:15 +0530

    feat(customer): Write unit test case for UserId

---
+++ services/novabank-customer-service/src/test/java/com/novabank/customer/domain/valueobject/identifier/UserIdTest.java
@@ -0,0 +1,32 @@
+package com.novabank.customer.domain.valueobject.identifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.UUID;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UserIdTest {
+
+    @Test
+    @DisplayName("Should create user id")
+    void shouldCreateUserId() {
+
+        UUID value = UUID.randomUUID();
+
+        UserId userId = new UserId(value);
+
+        assertThat(userId.value())
+            .isEqualTo(value);
+    }
+
+    @Test
+    @DisplayName("Should reject null value")
+    void shouldRejectNullValue() {
+
+        assertThatThrownBy(() -> new UserId(null))
+            .isInstanceOf(NullPointerException.class)
+            .hasMessage("UserId cannot be null");
+    }
+}
